6bbd08eeb7cef128b1a938b3cdcf380efc793546
Chen Chongsong <chenchongsong@outlook.com>
2020-03-11 17:20:34 +0800

    Avoid Hitting the wall during fastest path

--- src/driveStraight.ino
+++ src/driveStraight.ino
@@ -119,6 +119,12 @@ void goStraightFP(int grid) {
       powerLeft = power + correction;
     }
     md.setSpeeds((int)powerRight, (int)powerLeft);
+
+    if ((encoderPinRightTicks + encoderPinLeftTicks) / 2 + 100 >= distance) {
+      if (readIR2() < 5.55 && readIR3() < 6.0 && readIR4() < 4.7) {
+        break;
+      }
+    }
     // Serial.println("     " + String(encoderPinLeftTicks) +" | "+ String(encoderPinRightTicks));
     // Serial.println(String(diffValue) +":dif | correction:"+ String(correction));
   }
